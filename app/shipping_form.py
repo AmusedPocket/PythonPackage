fbb62d8f3f93d70336564a30bd590fa6d1b64e24
Lei Li <lei.xw.li@hotmail.com>
2024-02-08 02:04:14 -0800

    phase 1 done

--- app/shipping_form.py
+++ app/shipping_form.py
@@ -7,8 +7,8 @@ from map.map import map
 class Shipping_Form(FlaskForm):
     sender = StringField("Name of Sender", validators=[DataRequired()])
     recipent = StringField("Name of Recipient", validators=[DataRequired()])
-    origin = SelectField("Origin options", choices=[map.keys()], validators=[DataRequired()])
-    destination = SelectField("Destination options", choices=[map.keys()], validators=[DataRequired()])
+    origin = SelectField("Origin options", choices=[key for key in map.keys()], validators=[DataRequired()])
+    destination = SelectField("Destination options", choices=[key for key in map.keys()], validators=[DataRequired()])
     express_shipping = BooleanField("Express Shipping desired?", validators=[DataRequired()])
     submit_button = SubmitField("Submit")
     
